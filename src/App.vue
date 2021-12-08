c3e2459fad5651a9bf153873e650ddc8170e25dd
twilight943 <77723292+twilight943@users.noreply.github.com>
2021-12-08 11:37:37 +0800

    Add files via upload
    前端

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <el-container id="app">
+    <el-header>
+      温湿度自动检测系统
+    </el-header>
+    <el-main>
+      <router-view/>
+    </el-main>
+  </el-container>
+</template>
+
+<script>
+import webApp from "@/components/webApp"
+export default {
+  name: 'App',
+  components: {
+    webApp: webApp
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.el-header {
+  font-size: 30px;
+}
+</style>
