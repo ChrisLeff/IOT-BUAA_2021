0680f5772f6ba9d6b0ad21c634047458bb99267b
twilight943 <19373590@buaa.edu.cn>
2021-12-21 22:10:36 +0800

    Update app.py

--- iotFlaskPro/app.py
+++ iotFlaskPro/app.py
@@ -6,12 +6,13 @@ import apis.aep_device_status
 import apis.aep_device_command
 
 app = Flask(__name__)
+CORS(app, supports_credentials=True)
 
 body_base = '{ "productId": "15100261", "deviceId": "16749f24ae0e4d118f93b9d9eb2317b5"}'
 
 
 body_motor_control = \
-    '{"content": {"params": { "control_int": %s}, "serviceIdentifier": "motor_control" }, ' \
+    '{"content": {"params": { "set_int": %s}, "serviceIdentifier": "motor_control" }, ' \
     '"deviceId": "16749f24ae0e4d118f93b9d9eb2317b5", ' \
     '"operator": "BUAA202116", ' \
     '"productId": "15100261", ' \
@@ -25,29 +26,30 @@ def hello_world():  # put application's code here
     return 'Hello World!'
 
 
-@app.route('/get_temperature_humidity')
+@app.route('/get_temperature_humidity', methods=['POST', 'GET'])
 def get_temperature_humidity():
     result = apis.aep_device_status.QueryDeviceStatusList('D4RcZL9S9Sf', 'ifuketHPjc', body_base)
     postForm = json.loads(result)
-    return str (postForm['deviceStatusList'])
+    str1 = str(postForm['deviceStatusList']).replace("\'", "\"")
+    return str1
 
 
 @app.route('/get_history_status_page', methods=['POST', 'GET'])
 def get_history_status_page():
     if request.method == 'POST':
         print(request.form)
-        begin_timestamp = request.form.get('begin_timestamp')
-        end_timestamp = request.form.get('end_timestamp')
+        # begin_timestamp = request.form.get('begin_timestamp')
+        # end_timestamp = request.form.get('end_timestamp')
         # begin_timestamp = input()
         # end_timestamp = input()
-        # begin_timestamp = str(1637856052000)
-        # end_timestamp =   str(1637942452000)
-        body_history = '{"productId": "15100261",' \
-                       ' "deviceId": "16749f24ae0e4d118f93b9d9eb2317b5",' \
-                       ' "begin_timestamp":' + begin_timestamp + ',' \
-                       ' "end_timestamp":' + end_timestamp + ',' \
-                       ' "page_size": 200,' \
-                       ' "page_timestamp": null}'
+        begin_timestamp = str(1638954000000)
+        end_timestamp =   str(1638954600000)
+        body_history = '{"productId": "15100261", ' \
+                       '"deviceId":"16749f24ae0e4d118f93b9d9eb2317b5", ' \
+                       '"begin_timestamp":"' + begin_timestamp + '", ' \
+                       '"end_timestamp":"' + end_timestamp + '", ' \
+                       '"page_size":200, ' \
+                       '"page_timestamp": null}'
         result = apis.aep_device_status.getDeviceStatusHisInPage('D4RcZL9S9Sf', 'ifuketHPjc', body_history)
         postForm = json.loads(result)
         time = []
@@ -62,17 +64,17 @@ def get_history_status_page():
         total['timestamp'] = time
         total['temperature_data'] = temperature
         total['humidity_data'] = humidity
-        return str(total)
+        return str(total).replace("\'", "\"")
 
 
-@app.route('/turn_motor_on')
+@app.route('/turn_motor_on', methods=['POST', 'GET'])
 def turn_motor_on():
     result = apis.aep_device_command.CreateCommand('D4RcZL9S9Sf', 'ifuketHPjc', 'da940cf2c7484d59800d982b49ccecdd',
                                                    body_motor_control % "1")
     return result
 
 
-@app.route('/turn_motor_off')
+@app.route('/turn_motor_off', methods=['POST', 'GET'])
 def turn_motor_off():
     result = apis.aep_device_command.CreateCommand('D4RcZL9S9Sf', 'ifuketHPjc', 'da940cf2c7484d59800d982b49ccecdd',
                                                    body_motor_control % "0")
